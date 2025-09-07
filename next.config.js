516d311621a4002643790019b1708c819194c500
arkalogistics <arkalogistics8@gmail.com>
2025-09-07 00:25:31 -0600

    NEXT CONFIG

--- next.config.js
+++ next.config.js
@@ -2,9 +2,30 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    typedRoutes: true
-  }
+    typedRoutes: true,
+    // Avoid bundling native/dynamic @libsql bits into RSC/SSR build
+    serverComponentsExternalPackages: [
+      "@libsql/client",
+      "@prisma/adapter-libsql",
+      "libsql",
+      "@libsql/isomorphic-ws",
+      "@libsql/isomorphic-fetch",
+      "@libsql/hrana-client",
+      "@neon-rs/load",
+      "detect-libc",
+      "ws",
+      "node-fetch",
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Do not let Webpack try to crawl all files under @libsql/*
+      // which includes README.md, LICENSE, etc. Treat them as externals.
+      config.externals = config.externals || [];
+      config.externals.push(/^@libsql\/.*$/);
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
-
